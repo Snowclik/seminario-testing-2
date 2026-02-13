5f10d0f2dbc4f398ecf8bf2819b69432258ce8fb
snowclik <tavog4142@gmail.com>
2026-02-13 14:32:11 -0500

    cambios

--- script.test.js
+++ script.test.js
@@ -1,5 +1,5 @@
 import { test, expect } from "vitest"
-import { sum } from '../script.js'
+import { sum } from './script.js'
 
 test('suma incorrecta', () => {
     expect(sum(2, 2)).toBe(4);  // Este test fallará intencionalmente
